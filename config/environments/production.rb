68af32459d2b5f1ba4023240a54f249127232e98
Gary Marx <gary@zenpayroll.com>
2015-09-04 13:13:29 -0700

    setting up heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # required for Heroku
+  # Set for action host site, gm-rails.herokuapp.com
+  config.action_mailer.default_url_options = { :host => 'gm-rails.herokuapp.com' }
 end
